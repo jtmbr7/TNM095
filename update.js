577a9151ffabe3343b6bbb1f9d1bc8906c07593f
John <jtmbr7@outlook.com>
2022-09-20 22:58:53 +0200

    fiskarna finns

--- update.js
+++ update.js
@@ -1,27 +1,25 @@
 
-let myFishes = [];
-
+let fishes = [];
 function setup() {
-   
-    for(let i = 0; i < 14; i++)
-    {
-        min = Math.ceil(canvas.width);
-        max = Math.floor(canvas.height);
-        myFishes.push(new Fish(Math.random() * min, Math.random() * max, 25, Math.floor(Math.random() * 5 + 1)));
-    }    
+    
+    for(let i = 0; i < 2; ++i)
+    fishes.push(new Fish(new Position(300 + 100*i, 300), 20 + Math.random() * 10, 1));
 }
 
 function update() {
 
-    for (let i = 0; i < myFishes.length; i++)
-    {
-        myFishes[i].tick();
-        myFishes[i].draw();
-
-        for(let q = i + 1; q < myFishes.length; ++q) {
-            myFishes[i].collision(myFishes[q]);
+    for(let i = 0; i < fishes.length - 1; ++i) {
+        for(let q = i + 1; q < fishes.length; ++q) {
+            fishes[i].collision(fishes[q])
         }
     }
-    
+    fishes.forEach(fish => fish.tick());
+    fishes.forEach(fish => fish.draw());
 
+    if(key.d == "keydown")
+        fishes[0].angle += .1;
+    if(key.a == "keydown")
+        fishes[0].angle -= .1;
+    if(key.w == "keydown")
+        fishes[0].swim(.05);
 }
